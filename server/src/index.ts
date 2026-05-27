0d4c5430fa20f65b5cd9bb65c84464997858365e
David Beneito Pastor <glax.xyz@MacBook-Pro-de-David.local>
2026-05-27 22:53:28 +0200

    desploy changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://liminal-mgid.vercel.app/',
+    ],
+    credentials: true,
+}))
 app.use(express.json());
 
 // Rutas
